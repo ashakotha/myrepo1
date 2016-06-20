eb969739f8ee05b41d3d01dc2653fc2cb993912f
Jyothi Marisetty <jyothi.marisetty@tcs.com>
2016-06-20 08:59:52 -0400

    deleted divide method

--- ConsoleApplication2/calculatorlib/calculator.cs
+++ ConsoleApplication2/calculatorlib/calculator.cs
@@ -20,9 +20,6 @@ namespace calculatorlib
         {
             return a - b;
         }
-        public int divide(int a, int b)
-        {
-            return a / b;
-        }
+        
     }
 }
