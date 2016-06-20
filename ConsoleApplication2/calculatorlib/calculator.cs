13c0371051e4ebf8b6b2e9a498d1d5ce3b1871da
ashakotha <asha.kotha@tcs.com>
2016-06-20 06:15:14 -0400

    added sub and div classes

--- ConsoleApplication2/calculatorlib/calculator.cs
+++ ConsoleApplication2/calculatorlib/calculator.cs
@@ -16,5 +16,13 @@ namespace calculatorlib
         {
             return a * b;
         }
+        public int subtract(int a, int b)
+        {
+            return a - b;
+        }
+        public int divide(int a, int b)
+        {
+            return a / b;
+        }
     }
 }
